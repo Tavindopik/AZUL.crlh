73786555ea695da999e74b5621404d6bf00fe592
Tavindopik <dasuldajuh@gmail.com>
2024-10-28 08:56:24 -0300

    Add files via upload

---
+++ Class2.c
@@ -0,0 +1,34 @@
+//  Comentário
+/*Comentário com mais linhas
+*/
+// Biblioteca | Library
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//Tipos primitivos
+// int, float, char, bool
+
+// Variaveis de escopo Global 
+int numGlobal;
+
+
+// Função | Método principal
+int main(){
+		// Variaveis de escopo localeconv
+		float numQuebrado;
+		
+		// Inicializacao de variaveis de valores
+		char letter = 'a';
+		bool ativo = false;
+		
+		for(int i + 0; i <= 10; i++) {
+			printf("%d\n",i);
+		}
+}
+while(!ativo) {
+	printf("Olá, você esta está ativo") ;
+	ativo = true
+	
+    }
+}
